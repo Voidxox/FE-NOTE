cdeae5b5ff1f9d542c93a11a41008d0ca05b1a27
yangxin <yangxinovo@163.com>
2023-04-19 16:15:55 +0800

    base

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  base:'/FE-NOTE/',
   title: "Void的文档",
   description: "A VitePress Site",
   themeConfig: {
