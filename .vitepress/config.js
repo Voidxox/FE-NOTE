4a25ae43865daea026c07ef0bc0e1cac010a1dec
yangxin <yangxinovo@163.com>
2023-04-19 16:01:27 +0800

    first

---
+++ .vitepress/config.js
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "Void的文档",
+  description: "A VitePress Site",
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    // nav: [
+    //   { text: 'Home', link: '/' },
+    //   { text: 'Examples', link: '/markdown-examples' }
+    // ],
+
+    sidebar: [
+      {
+        text: '前端',
+        items: [
+          { text: 'learn', link: '/fe' },
+          // { text: 'Runtime API Examples', link: '/api-examples' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/Voidxox' }
+    ]
+  }
+})
